585bcfec968c3deb141bfe2977ae106f245c361f
andreajhdez <85519006+andreajhdez@users.noreply.github.com>
2023-10-30 00:05:19 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#Se pide el nombre del jugador por teclado. Se declara variable nom_jugador, para almacenar esta información. 
+nom_jugador = input('Ingresa tu nombre: ')
+
+#Se imprime el mensaje de bienvenido con el nombre del jugador (nom_jugador)
+print(f'Hola {nom_jugador}, bienvenidx al Laberinto mas oscuro y retador que podrás encontrar... ¿Estás listx para iniciar este reto?')
