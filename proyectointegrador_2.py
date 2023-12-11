2ba05d5a8f3f3a8ee98577340a11101602b735ed
andreajhdez <85519006+andreajhdez@users.noreply.github.com>
2023-12-11 18:07:45 -0500

    Update proyectointegrador_2.py

--- proyectointegrador_2.py
+++ proyectointegrador_2.py
@@ -3,6 +3,9 @@ from readchar import readkey, key
 # inicializamos el bucle infinito, poniendo el while siempre verdadero.
 while True:
     k = readkey()
-    # Si se oprime la tecla hacia arriba se sale del bucle infinito.
+    # Si se oprime la tecla hacia arriba se sale del bucle infinito y si se oprime cualquier tecla del teclado imprime las teclas en consola.
+    if k != key.UP:
+        print(k)
     if k == key.UP:
         break
+
